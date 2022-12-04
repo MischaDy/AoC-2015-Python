dbdb974ad133307ac07e7cde2eab714555597a12
Mischa <MischaDy@users.noreply.github.com>
2022-12-04 23:53:31 +0100

    Changed base file

--- DayX/dayx.py
+++ DayX/dayx.py
@@ -7,22 +7,22 @@ INPUT_PATH = 'input.txt'
 
 def main(run_test, part, test_input_path, input_path):
     file_path = test_input_path if run_test else input_path
-    day_function = dayX_part1 if part == 1 else dayX_part2
+    day_function = run_part1 if part == 1 else run_part2
     input_ = get_input(file_path)
     print(day_function(input_))
 
 
-def dayX_part1(input_):
+def run_part1(input_):
     pass
 
 
-def dayX_part2(input_):
+def run_part2(input_):
     pass
 
 
 def get_input(file_path):
     with open(file_path) as f:
-        input_ = ...
+        input_ = f.read().split('\n')
 
     return input_
 
