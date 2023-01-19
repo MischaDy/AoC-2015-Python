d7a0cfb1ddb9f7dcf490fd2318dea84ad4503a48
Mischa <MischaDy@users.noreply.github.com>
2023-01-19 19:30:45 +0100

    general stuffs

--- DayX/main.py
+++ DayX/main.py
@@ -8,7 +8,7 @@ INPUT_PATH = 'input.txt'
 def main(run_test, part, test_input_path, input_path):
     file_path = test_input_path if run_test else input_path
     day_function = run_part1 if part == 1 else run_part2
-    input_ = get_input(file_path)
+    input_ = get_input(file_path, line_sep='\n')
     print(day_function(input_))
 
 
@@ -20,10 +20,12 @@ def run_part2(input_):
     pass
 
 
-def get_input(file_path):
+def get_input(file_path, line_sep=None):
     with open(file_path) as f:
-        input_ = f.read().split('\n')
+        input_ = f.read()
 
+    if line_sep is not None:
+        input_ = input_.split(line_sep)
     return input_
 
 
